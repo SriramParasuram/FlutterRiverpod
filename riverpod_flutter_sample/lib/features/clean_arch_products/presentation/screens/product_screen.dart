063939391dcee737faa799f38d9c3209c2a853cd
Sriram Parasuram <sriramsp88@gmail.com>
2025-07-19 18:21:54 +0530

    new test log added in main

--- riverpod_flutter_sample/lib/features/clean_arch_products/presentation/screens/product_screen.dart
+++ riverpod_flutter_sample/lib/features/clean_arch_products/presentation/screens/product_screen.dart
@@ -21,6 +21,7 @@ class _ProductListScreenState extends ConsumerState<ProductListScreen> {
     print("build method called!");
     print("test log added in main branch");
     print("test log added in checkout test branch");
+    print("new test log added in main branch");
     final productAsyncValue = ref.watch(getAllProductsProvider);
 
     return Scaffold(
